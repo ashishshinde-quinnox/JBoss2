4f05cae3b4766c4a7f23dcf16d1fa70a39751284
AshishShinde <AshishShinde@quinnox.com>
2015-02-04 16:21:18 +0530

    Adding class Addition

---
+++ jbossdemo/demo/calculator/Addition.java
@@ -0,0 +1,12 @@
+package demo.calculator;
+
+public class Addition {
+
+	public int add(int i, int j) {
+		
+		int k = i + j;
+		System.out.println("demo.calculator.Addition: add :" + k );
+		return k;
+	}
+	
+}
