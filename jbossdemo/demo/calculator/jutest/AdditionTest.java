504f4c96afa8847642c838a3c253cdd9791597b7
AshishShinde <AshishShinde@quinnox.com>
2015-02-04 19:27:40 +0530

    Added test class for Addition

---
+++ jbossdemo/demo/calculator/jutest/AdditionTest.java
@@ -0,0 +1,14 @@
+package demo.calculator.jutest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdditionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
